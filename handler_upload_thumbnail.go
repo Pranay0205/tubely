93108f67c1b00dd78c765eb142ba43a7ee5a09e1
Pranay0205 <pranayghuge@gmail.com>
2025-08-24 11:51:41 -0400

    feat: remove in memory thumbnail and video upload with database base64encoded storage in SQL

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +31,58 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+
+	b, err := io.ReadAll(file)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Coudn't read the video file", err)
+		return
+	}
+
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusNotFound, "Video doesn't exists", err)
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video from database", err)
+		return
+	}
+
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Invalid User ID", err)
+		return
+	}
+
+	base64Encoded := base64.StdEncoding.EncodeToString(b)
+
+	dataURL := "data:" + contentType + ";base64," + base64Encoded
+
+	videoData.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+
+		respondWithError(w, http.StatusInternalServerError, "Unable to save video to database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
+
 }
